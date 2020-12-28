2d49b2f54796e781e74c96aeba395f8ab61cbfa5
Zezhou.Sun <sun_zezhou@njust.edu.cn>
2020-12-28 18:44:48 +0800

    Site updated: 2020-12-28 18:44:47

--- js/botui.js
+++ js/botui.js
@@ -26,7 +26,7 @@ function bot_ui_ini() {
     }).then(function () {
         botui.message.add({
             delay: 1100,
-            content: "我是聿舟"
+            content: "这里是 Mashiro"
         }).then(function () {
             botui.message.add({
                 delay: 1100,
@@ -65,90 +65,90 @@ function bot_ui_ini() {
         secondpart = function () {
             botui.message.add({
                 delay: 1500,
-                content: "目前就读于南京理工大学"
+                content: "目前就读于上海财经大学"
             }).then(function () {
                 botui.message.add({
                     delay: 1500,
-                    content: "现在博三正在为发论文而秃头中..."
+                    content: "向往技术却误入商科，但后来喜欢上了经济学…"
                 }).then(function () {
                     botui.message.add({
                         delay: 1200,
-                        content: "主要研究方向是机器人SLAM"
+                        content: "因为数据分析也需要Coder嘛"
                     }).then(function () {
                         botui.message.add({
                             delay: 1500,
-                            content: "主攻 Lidar SLAM 方法, 对 Motion and Path Planning, Vision SLAM, Img/Video Stiting 方向也略有涉猎。"
+                            content: "主攻 R 语言和 Python，略懂 STATA，偶尔也折腾 HTML/CSS/JavaScript/PHP"
                         }).then(function () {
                             botui.message.add({
                                 delay: 1500,
-                                content: "未来想再了解一些 DL/RL 方向下的 Active SLAM 和主动检测分割"
+                                content: "研究的方向，是经济/金融方向的数据分析（data science）以及机器学习（machine learning）"
                             }).then(function () {
                                 botui.message.add({
                                     delay: 1800,
-                                    content: "如果喜欢我的研究与工作"
+                                    content: "喜欢画画，希望有一天能够被称为画师"
                                 }).then(function () {
                                     botui.action.button({
                                         delay: 1100,
                                         action: [{
-                                            text: "欢迎参观我的Github～🤔",
-                                            value: "https://github.com/SunZezhou"
+                                            text: "为什么叫Mashiro呢？ 🤔",
+                                            value: "why-mashiro"
                                         }]
-                                    })//.then(function (a) {
-                                     //   thirdpart()
-                                    //})
+                                    }).then(function (a) {
+                                        thirdpart()
+                                    })
                                 })
                             })
                         })
                     })
                 })
             })
-         }//,
-        // thirdpart = function () {
-        //     botui.message.add({
-        //         delay: 1E3,
-        //         content: "Mashiro以及站名都来自一部动画，因为和主角有一样的爱好~ 如果有兴趣可以找找首页上的视频~"
-        //     }).then(function () {
-        //         botui.action.button({
-        //             delay: 1500,
-        //             action: [{
-        //                 text: "为什么是白猫呢？ 🤔",
-        //                 value: "why-cat"
-        //             }]
-        //         }).then(function (a) {
-        //             fourthpart()
-        //         })
-        //     })
-        // },
-        // fourthpart = function () {
-        //     botui.message.add({
-        //         delay: 1E3,
-        //         content: "因为对GitHub有种执念… "
-        //     }).then(function () {
-        //         botui.message.add({
-        //             delay: 1100,
-        //             content: "而且我真的是猫控！"
-        //         }).then(function () {
-        //             botui.action.button({
-        //                 delay: 1500,
-        //                 action: [{
-        //                     text: "域名有什么含意吗？(ง •_•)ง",
-        //                     value: "why-domain"
-        //                 }]
-        //             }).then(function (a) {
-        //                 fifthpart()
-        //             })
-        //         })
-        //     })
-        // },
-        // fifthpart = function () {
-        //     botui.message.add({
-        //         delay: 1E3,
-        //         content: "emmmm，看备案信息你就知道了=.= 本来想要zheng.xin的，但50万真买不起。。"
-        //     }).then(function () {
-        //         botui.message.add({
-        //             delay: 1600,
-        //             content: "那么，仔细看看我的博客吧？ ^_^"
-        //         })
-        //     })
-        // } 
+        },
+        thirdpart = function () {
+            botui.message.add({
+                delay: 1E3,
+                content: "Mashiro以及站名都来自一部动画，因为和主角有一样的爱好~ 如果有兴趣可以找找首页上的视频~"
+            }).then(function () {
+                botui.action.button({
+                    delay: 1500,
+                    action: [{
+                        text: "为什么是白猫呢？ 🤔",
+                        value: "why-cat"
+                    }]
+                }).then(function (a) {
+                    fourthpart()
+                })
+            })
+        },
+        fourthpart = function () {
+            botui.message.add({
+                delay: 1E3,
+                content: "因为对GitHub有种执念… "
+            }).then(function () {
+                botui.message.add({
+                    delay: 1100,
+                    content: "而且我真的是猫控！"
+                }).then(function () {
+                    botui.action.button({
+                        delay: 1500,
+                        action: [{
+                            text: "域名有什么含意吗？(ง •_•)ง",
+                            value: "why-domain"
+                        }]
+                    }).then(function (a) {
+                        fifthpart()
+                    })
+                })
+            })
+        },
+        fifthpart = function () {
+            botui.message.add({
+                delay: 1E3,
+                content: "emmmm，看备案信息你就知道了=.= 本来想要zheng.xin的，但50万真买不起。。"
+            }).then(function () {
+                botui.message.add({
+                    delay: 1600,
+                    content: "那么，仔细看看我的博客吧？ ^_^"
+                })
+            })
+        } 
 }
